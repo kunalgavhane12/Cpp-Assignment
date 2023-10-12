abb31b9560a6195e8412f33c7aebab44610ec5f6
kunalgavhane12 <kunalgavhane12@gmail.com>
2023-10-12 11:30:57 +0530

    code

---
+++ 5.cpp
@@ -0,0 +1,32 @@
+/******************************************************************************
+C++ Program for Remove all duplicates from the input string.
+******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+string removeAllDuplicate(string s)
+{
+   string temp="";
+   int arr[128]={0};
+   for(int i=0; i<s.length(); i++)
+   {
+       if(arr[s[i]]==0)
+       {
+           arr[s[i]]=1;
+           temp += s[i];
+       }
+   }
+   return temp;
+}
+
+int main()
+{
+    string str;
+    cout<<"Enter the string:";
+    getline(cin,str);
+    cout<<removeAllDuplicate(str)<<endl;
+
+    return 0;
+}
+
